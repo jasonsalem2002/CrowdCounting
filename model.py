db3ba195f39036016dfd23451e9abafed951c1c2
jasonsalem2002 <jason.salem19@hotmail.com>
2024-04-04 23:21:09 +0300

    Added the model

---
+++ model.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+from torchvision import models
+
+class SimplifiedCSRNet(nn.Module):
+    def __init__(self):
+        super(SimplifiedCSRNet, self).__init__()
+        self.frontend_features = models.vgg16(pretrained=True).features[:23]
+        self.backend = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 1, kernel_size=1),
+        )
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.frontend_features(x)
+        x = self.backend(x)
+        return x
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, std=0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
